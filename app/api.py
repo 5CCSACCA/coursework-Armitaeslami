b4e11c831bfaef44d018232710af8240553bec82
Armita Eslami Nazari <armita.eslami_nazari@kcl.ac.uk>
2025-12-05 12:57:20 +0000

    stage 4 is done , Add MongoDB persistence, history endpoint, docker-compose setup

--- app/api.py
+++ app/api.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
+import shutil
+
 from app.services.yolo_service import YoloService
 from app.services.llm_service import LlmService
-import shutil
+from app.services.db_service import DatabaseService
 
 app = FastAPI()
 
-# make the services just once
+# create the services once
 yolo_service = YoloService()
 llm_service = LlmService()
+db_service = DatabaseService()
 
 @app.get("/")
 def home():
@@ -15,30 +18,31 @@ def home():
 
 @app.post("/detect")
 async def detect_objects(file: UploadFile = File(...)):
-    # save the uploaded file to a temporary place
     file_location = "temp_image.jpg"
     with open(file_location, "wb") as f:
         shutil.copyfileobj(file.file, f)
 
-    # run YOLO detection
     results = yolo_service.detect_objects(file_location)
-
     return {"objects": results}
 
 @app.post("/describe")
 async def describe_image(file: UploadFile = File(...)):
-    # save file again
     file_location = "temp_image.jpg"
     with open(file_location, "wb") as f:
         shutil.copyfileobj(file.file, f)
 
-    # YOLO detection
     results = yolo_service.detect_objects(file_location)
-
-    # LLM description
     description = llm_service.generate_description(results)
+
+    # save record in mongo
+    db_service.save_record(results, description)
 
     return {
         "objects": results,
         "description": description
     }
+
+@app.get("/history")
+def get_history():
+    records = db_service.get_all_records()
+    return {"history": records}
